2b2697ec7ba98e489208e7c25e6ef2b411bce921
K P <31292474+kpalmberg@users.noreply.github.com>
2022-09-16 22:43:28 -0700

    Added linter action, and a simple .NET project to test it.

---
+++ src/dotnet-lint-testing/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace dotnet_lint_testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+        }
+    }
+}
